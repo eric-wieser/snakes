ccdb2321ed89b7b4634a93f7350e369c05002f6f
Eric Wieser <wieser.eric@gmail.com>
2012-05-03 09:23:36 +0100

    Added the newer version of vector.js, and a Color class

--- vector.js
+++ vector.js
@@ -30,11 +30,18 @@ Vector.prototype = {
 	plus: function(that) {
 		return new Vector(this.x + that.x, this.y + that.y);
 	},
+	plusEquals: function(that) {
+		this.x += that.x;
+		this.y += that.y;
+		return this;
+	},
 	minus: function(that) {
-		if(!that)
-			return new Vector(-this.x, -this.y);
-		else
-			return new Vector(this.x - that.x, this.y - that.y);
+		return new Vector(this.x - that.x, this.y - that.y);
+	},
+	minusEquals: function(that) {
+		this.x -= that.x;
+		this.y -= that.y;
+		return this;
 	},
 	times: function(factor) {
 		if(factor instanceof Vector)
@@ -42,17 +49,48 @@ Vector.prototype = {
 		else
 			return new Vector(this.x * factor, this.y * factor);
 	},
+	timesEquals: function(factor) {
+		if(factor instanceof Vector) {
+			this.x *= factor.x;
+			this.y *= factor.y;
+		} else {
+			this.x *= factor;
+			this.y *= factor;
+		}
+		return this;
+	},
 	over: function(factor) {
 		return new Vector(this.x / factor, this.y / factor);
+	},
+	overEquals: function(factor) {
+		if(factor instanceof Vector) {
+			this.x /= factor.x;
+			this.y /= factor.y;
+		} else {
+			this.x /= factor;
+			this.y /= factor;
+		}
+		return this;
+	},
+	negated: function() {
+		return new Vector(-this.x, -this.y);
+	},
+	negate: function() {
+		this.x = -this.x;
+		this.y = -this.y;
+		return this;
 	},
 	dot: function(that) {
 		return this.x * that.x + this.y * that.y;
 	},
 	distanceTo: function(that) {
-		return this.subtract(that).magnitude();
+		return this.minus(that).magnitude();
+	},
+	angleTo: function(that) {
+		return Math.acos(this.dot(that) / (this.magnitude() * that.magnitude()))
 	},
 	lerp: function(that, t) {
-		return that.times(t).plus(this.times(1-t));
+		return that.times(t).plus(this.times(1 - t));
 	},
 	perp: function() {
 		return new Vector(-this.y, this.x);
@@ -68,6 +106,17 @@ Vector.prototype = {
 	}
 };
 
-Vector.zero = new Vector(0,0);
-Vector.i = new Vector(1,0);
-Vector.j = new Vector(0,1);
+Vector.zero = function() { return new Vector(0, 0) };
+Vector.i = function() { return new Vector(1, 0) };
+Vector.j = function() { return new Vector(0, 1) };
+Vector.NaN = function() { return new Vector(Number.NaN, Number.NaN) };
+
+Vector.prototype['+'] = Vector.prototype.plus
+Vector.prototype['-'] = Vector.prototype.minus
+Vector.prototype['*'] = Vector.prototype.times
+Vector.prototype['/'] = Vector.prototype.over
+
+Vector.prototype['+='] = Vector.prototype.plusEquals
+Vector.prototype['-='] = Vector.prototype.minusEquals
+Vector.prototype['*='] = Vector.prototype.timesEquals
+Vector.prototype['/='] = Vector.prototype.overEquals
