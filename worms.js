b1603bb83c455c773786ec68ae3d376b7c617418
Eric Wieser <wieser.eric@gmail.com>
2012-05-03 09:44:19 +0100

    removed internal copy of color.js inside worms.js

--- worms.js
+++ worms.js
@@ -43,40 +43,7 @@ var keycodes = {
 	right: [68, 39]
 }
 
-function Color(r, g, b) {
-	this.r = Color.clipComponent(r);
-	this.g = Color.clipComponent(g);
-	this.b = Color.clipComponent(b);
-}
-Color.prototype.randomNear = function(range) {
-	return new Color(
-		randomInt(Color.clipComponent(this.r - range), Color.clipComponent(this.r + range)),
-		randomInt(Color.clipComponent(this.g - range), Color.clipComponent(this.g + range)),
-		randomInt(Color.clipComponent(this.b - range), Color.clipComponent(this.b + range))
-	)
-};
-Color.prototype.lerp = function(that, x) {
-	var y = 1 - x;
-	return new Color(
-		this.r*y + that.r*x,
-		this.g*y + that.g*x,
-		this.b*y + that.b*x
-	);
-};
-Color.prototype.inverted = function() {
-	return new Color(
-		255-this.r, 255-this.g, 255-this.b
-	);
-}
-Color.prototype.toString = function() {
-	return 'rgb(' + this.r + ', ' + this.g + ', ' + this.b + ')';
-};
-Color.random = function(r, g, b, range) {
-	return new Color(r,g,b).randomNear(range);
-}
-Color.clipComponent = function(x) {
-	return x > 255 ? 255 : x < 0 ? 0 : x;
-}
+
 function Ball(pos, radius, color, pole) {
 	this.position = pos;
 	this.velocity = new Vector(0, 0);
