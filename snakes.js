945b05149286bf440210e3cfa944e9d89896a1f2
Eric Wieser <wieser.eric@gmail.com>
2012-05-04 22:44:19 +0100

    Added comments

--- snakes.js
+++ snakes.js
@@ -136,62 +136,15 @@ Snake.prototype.update = function(dt) {
 	if(last.getMass() < rate) {
 		this.balls.pop();
 	}
-	/*
-	var len = this.balls.length
-	if(len > 10) {
-		var lose = 1;
-		var last = this.balls[len-1];
-		last.radius -= lose;
-		if(last.radius <= 0) this.balls.pop();
-
-		this.balls.forEach(function(ball) {
-			if(ball != last)
-				ball.radius += lose / len;
-		})
-	}*/
-	/*var last = this.balls[this.balls.length - 1];
-	var eachTime = 0.1;
-	this.balls.forEach(function(a, i) {
-		var aMass = a.getMass();
-		var neededMass = this.maxMass - aMass;
-		var massLeft = Math.abs(neededMass * eachTime);
-		var massAdded = 0;
-		for(var j = i + 1; j < this.balls.length; j++) {
-			var b = this.balls[i];
-			var m = b.getMass();
-			var toTake = Math.min(massLeft, m, 10);
-			if(neededMass > 0)
-				b.setMass(m - toTake);
-			else
-				b.setMass(m + toTake);
-
-			massLeft -= toTake;
-			massAdded += toTake;
-		}
-		if(neededMass > 0)
-			a.setMass(aMass + massAdded);
-		else
-			a.setMass(aMass - massAdded);
-
-	}, this);
-	var theMass = last.getMass()
-	if(theMass > this.maxMass) {
-		var newTail = new Ball(this.head.position.clone(), 0, last.color);
-		this.balls.push(newTail);
-		last.setMass(theMass - 10)
-		newTail.setMass(10)
-	} else if (last.getMass() < 0) {
-		this.balls.pop();
-	}*/
-
-
+	
 	//Physics on the head
 	this.balls[0].update(dt);
 	this.balls[0].bounceOffWalls(width, height);
 	this.balls[0].updateForceFrom(this.balls);
 
-	//Iterate down the body
+	//Iterate down the body - TODO check efficiency and order
 	this.balls.forAdjacentPairs(function(bi, bj, i, j) {
+		//interact with non-adjacent balls in the chain
 		for(var k = 1; k < this.balls.length; k++) {
 			if(k > j+1 || k < j - 1)
 				this.balls[j].updateForceFrom(this.balls[k]);
@@ -204,9 +157,12 @@ Snake.prototype.update = function(dt) {
 
 	//Interactions with free balls
 	balls.forEach(function(ball, i) {
+		//eat ones touching the head
 		if(ball.touches(this.head) && this.eat(ball)) {
 			balls.splice(i, 1)[0];
-		} else {
+		}
+		//bounce off the rest
+		else {
 			this.balls.forEach(function(b) {
 				b.updateForceFrom(ball);
 			});
@@ -219,26 +175,28 @@ Snake.prototype.update = function(dt) {
 		if(that == this) return;
 		this.balls.forEach(function(segment1) {
 			that.balls.forEach(function(segment2, index) {
-				segment1.updateForceFrom(segment2);
-				if(segment1 == this.head && segment2 != that.head && this.head.touches(segment2)) {
-					if(this.eat(segment2)) {
-						var removed = that.balls.splice(index);
-						removed.shift();
-						if(removed.length > that.balls.length) {
-							var r = that.balls;
-							that.balls = removed.reverse();
-							that.balls[0].color = that.head.color;
-							that.balls[0].radius = that.head.radius;
-							that.head = that.balls[0];
-							removed = r;
-						}
-						removed.forEach(function(b) {
-							balls.push(b);
-							b.forces = {};
-							b.forces.contact = {};
-						});
+				//Eat and split if the head makes contact
+				if(segment1 == this.head && segment2 != that.head && this.head.touches(segment2) && this.eat(segment2)) {
+					var removed = that.balls.splice(index);
+					removed.shift();
+					if(removed.length > that.balls.length) {
+						var r = that.balls;
+						that.balls = removed.reverse();
+						that.balls[0].color = that.head.color;
+						that.balls[0].radius = that.head.radius;
+						that.head = that.balls[0];
+						removed = r;
 					}
-				};
+					removed.forEach(function(b) {
+						balls.push(b);
+						b.forces = {};
+						b.forces.contact = {};
+					});
+				}
+				//Else, update forces
+				else {
+					segment1.updateForceFrom(segment2);
+				}
 			}, this);
 		}, this);
 	}, this);
@@ -246,38 +204,31 @@ Snake.prototype.update = function(dt) {
 
 };
 
+//Generate the gray balls
 for(var i = 0; i <= 50; i++) {
 	var r = Math.random();
-	var color, radius
+	var color, radius;
 
-	if(r < 0.33)
-		color = new Color(192, 192, 192), radius = randomInt(5,10);
-	else if(r < 0.66)
-		color = new Color(128, 128, 128), radius = randomInt(10, 20);
-	else
-		color = new Color(64, 64, 64), radius = randomInt(20,40);
+	if     (r < 0.33) color = new Color(192, 192, 192), radius = randomInt(5,  10);
+	else if(r < 0.66) color = new Color(128, 128, 128), radius = randomInt(10, 20);
+	else              color = new Color( 64,  64,  64), radius = randomInt(20, 40);
 
 	balls[i] = new Ball(new Vector(randomInt(width), randomInt(height)), radius, color);
 }
 
-snakes[0] = new Snake(10, new Color(255, 128, 0), new Vector(width/3, height / 2));
+//Add the two snakes
+snakes[0] = new Snake(10, new Color(255, 128, 0), new Vector(  width/3, height / 2));
 snakes[1] = new Snake(10, new Color(0, 128, 255), new Vector(2*width/3, height / 2));
 
+//Queue animation frames
 var lastt = Date.now();
 var lastdrawt = lastt;
-
-var bluescore = $('#blue-score');
-var orangescore = $('#orange-score');
-setInterval(function() {
-	orangescore.text(Math.round(snakes[0].getMass() / 500));
-	bluescore.text(Math.round(snakes[1].getMass() / 500));
-}, 250);
-
 function draw(t) {
+	//Calculate frame time
 	var dt = (t - lastt) / 1000.0;
 	if(dt > 0.2) dt = 0.2;
-	//ctx.clearRect(0, 0, canvas.width, canvas.height)
-	
+
+	//Update physics of all the balls and snakes
 	balls.forEach(function(b1, i) {
 		for(var j = i+1; j <= balls.length - 1; j++) {
 			var b2 = balls[j];
@@ -290,20 +241,27 @@ function draw(t) {
 	snakes[0].update(dt);
 	snakes[1].update(dt);
 
-	ctx.globalCompositeOperation = "source-over";
+	//draw the black background
 	//ctx.clearRect(0, 0, width, height);
+	ctx.globalCompositeOperation = "source-over";
 	ctx.fillStyle = "black";
 	ctx.fillRect(0, 0, width, height);
+
+	//draw all the things
 	ctx.globalCompositeOperation = "lighter";
 	balls.forEach(function(ball) {
 		ball.drawTo(ctx);
 	});
 	snakes[0].drawTo(ctx);
 	snakes[1].drawTo(ctx);
+
+	//prepare for next frame
 	lastt = t;
 	requestAnimationFrame(draw);
 }
 requestAnimationFrame(draw);
+
+//Handle keypresses
 $(window).keydown(function(e) {
 	keycodes.forEach(function(k, i) {
 		var h = snakes[i].head;
@@ -314,8 +272,7 @@ $(window).keydown(function(e) {
 		if(k.left  == e.which) h.forces.player.x = -a;
 		if(k.right == e.which) h.forces.player.x = a;
 	});
-})
-$(window).keyup(function(e) {
+}).keyup(function(e) {
 	keycodes.forEach(function(k, i) {
 		var h = snakes[i].head;
 		if(!("player" in h.forces)) h.forces.player = Vector.zero()
@@ -326,3 +283,11 @@ $(window).keyup(function(e) {
 		if(k.right == e.which) h.forces.player.x = 0;
 	});
 })
+
+//Show the scores
+var bluescore = $('#blue-score');
+var orangescore = $('#orange-score');
+setInterval(function() {
+	orangescore.text(Math.round(snakes[0].getMass() / 500));
+	bluescore.text(Math.round(snakes[1].getMass() / 500));
+}, 250);
